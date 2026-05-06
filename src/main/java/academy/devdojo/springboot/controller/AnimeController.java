65879d31ad06495599b0e8ba6a42b7c43afa47f3
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-05-06 20:46:02 -0300

    Metodo Delete em Spring

--- src/main/java/academy/devdojo/springboot/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -36,4 +36,11 @@ public class AnimeController {
     public ResponseEntity<Anime> save(@RequestBody Anime anime){
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
+
+    //Delete e id Potent -> Ver mais sobre isso
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
