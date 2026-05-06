d8756f5bdefbb440d5fc7622c9369fb5180e8bc9
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-05-06 20:26:26 -0300

    Metodo POST em SB

--- src/main/java/academy/devdojo/springboot/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import academy.devdojo.springboot.dominio.Anime;
 import academy.devdojo.springboot.util.DateUtil;
 
@@ -21,7 +18,7 @@ import java.util.List;
 @Log4j2
 public class AnimeController {
 
-    private final DateUtil dateUtil; // 👈 TEM QUE SER FINAL
+    private final DateUtil dateUtil;
     private final AnimeService animeService;
 
     @GetMapping
@@ -33,5 +30,10 @@ public class AnimeController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> finById(@PathVariable("id") long id) {
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 }
--- src/main/java/academy/devdojo/springboot/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot/service/AnimeService.java
@@ -6,11 +6,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService{
-    private List<Anime> animes = List.of(new Anime(1L,"Re:Zero"), new Anime(2L,"Berserk"));
+    private List<Anime> animes;
+
+    {
+        animes = new ArrayList<>(List.of(new Anime(1L,"Re:Zero"), new Anime(2L,"Berserk")));
+    }
 
     public List<Anime> listAll(){
         return animes;
@@ -21,5 +27,11 @@ public class AnimeService{
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 3000));
+        animes.add(anime);
+        return anime;
     }
 }
