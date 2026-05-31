c48b60c85f3435f0604f84bf82b9053b18ed4464
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-05-31 20:57:33 -0300

    RestTemplate Methods

--- src/main/java/academy/devdojo/springboot/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot/client/SpringClient.java
@@ -3,16 +3,14 @@ package academy.devdojo.springboot.client;
 import academy.devdojo.springboot.dominio.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Log4j2
 public class SpringClient {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //Temos tambem o RestTemplate().(method)forEntity... Ex: GetForEntity
+
         ResponseEntity<Anime> entity = new RestTemplate().exchange(
                 "http://localhost:8080/animes/2",
                 HttpMethod.GET,
@@ -22,14 +20,65 @@ public class SpringClient {
         log.info(entity.getStatusCode());
         log.info(entity.getBody());
 
-        ResponseEntity<Anime> animes = new RestTemplate().exchange(
+        ResponseEntity<Anime> exchange = new RestTemplate().exchange(
                 "http://localhost:8080/animes/2",
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<>() {}
+                new ParameterizedTypeReference<>() {
+                }
         );
-        log.info(animes.getStatusCode());
-        log.info(animes.getBody());
-        log.info(animes);
+        log.info(exchange.getStatusCode());
+        log.info(exchange.getBody());
+        log.info(exchange);
+
+//        Anime request =
+//                Anime.builder()
+//                        .name("Naruto")
+//                        .build();
+//        Anime animeSaved = new RestTemplate().postForObject("http://localhost:8080/animes", request, Anime.class);
+//        log.info("Anime saved: {}", animeSaved);
+
+
+        //O exchange pode ajudar no envio do ResponseEntity que pode ajudar a visualizar e manipular regras Http
+        Anime request =
+                Anime.builder()
+                        .name("Naruto")
+                        .build();
+        ResponseEntity<Anime> animeSaved = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.POST,
+                new HttpEntity<>(request),
+                Anime.class);
+        log.info("Anime saved: {}", animeSaved);
+
+        //-------------------------------------------
+
+//        ParameterizedTypeReference serve para preservar informações de tipos genéricos em tempo de execução.
+
+
+//        ResponseEntity<Void> animeUpdate = new RestTemplate().exchange(
+//                "http://localhost:8080/animes",
+//                HttpMethod.PUT,
+//                new HttpEntity<>(animeSaved, createJSONHeaders()),
+//                Void.class
+//        );
+//        log.info(animeUpdate);
+
+        //-------------------------
+
+        assert animeSaved.getBody() != null;
+        ResponseEntity<Void> animeUpdate = new RestTemplate().exchange(
+                "http://localhost:8080/animes/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class,
+                animeSaved.getBody().getId()
+        );
+        log.info(animeUpdate);
+    }
+
+    private static HttpHeaders createJSONHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
