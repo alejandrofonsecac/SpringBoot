4d35bbe341beeb87f673d093d5868398b8a22c15
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-06-01 20:40:56 -0300

    Requests for AnimeControllerTest

--- src/main/java/academy/devdojo/springboot/request/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot/request/AnimePostRequestBody.java
@@ -2,9 +2,11 @@ package academy.devdojo.springboot.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
     //Validações podendo ser feitas com o spring-boot-starter-validation
     @NotNull(message = "The anime name cannot be null")
---
+++ src/test/java/util/AnimePutRequestBodyCreator.java
@@ -0,0 +1,13 @@
+package util;
+
+import academy.devdojo.springboot.request.AnimePostRequestBody;
+import academy.devdojo.springboot.request.AnimePutRequestBody;
+
+public class AnimePutRequestBodyCreator {
+    public static AnimePutRequestBody createAnimePutRequestBody(){
+        return AnimePutRequestBody.builder()
+                .id(AnimeCreator.createValidUpdatedAnime().getId())
+                .name(AnimeCreator.createValidUpdatedAnime().getName())
+                .build();
+    }
+}
